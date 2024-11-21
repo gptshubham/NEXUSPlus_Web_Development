cf7faeb77fd45a9faec284768625c1118cc81cdd
gptshubham <shubhamca76@gmail.com>
2024-11-21 09:24:36 +0530

    introduction to JavaScript completed

---
+++ wd03_JavaScript/day01-Intro_to_JavaScript/script.js
@@ -0,0 +1,23 @@
+console.log("Hello Coder Army");
+
+// let const var
+
+// var --> outdated (avoid at all cost)
+
+let num = 10;
+console.log(num);
+
+let ar = "SKG";
+console.log(ar);
+ar = "Shubham";
+console.log(ar);
+
+const ids = 20;
+console.log(ids);
+// ids = 30;
+// TypeError: Assignment to constant variable.
+
+var x = 10;
+console.log(x);
+x = 20;
+console.log(x);
