81d795c13e284ac27dd35b969fe99547955e907c
gptshubham <shubhamca76@gmail.com>
2024-11-22 08:15:43 +0530

    primitive data types completed

---
+++ wd03_JavaScript/day02_data_types_in_javascript/script.js
@@ -0,0 +1,48 @@
+// Variable Declaration
+/*
+let num = 10;
+console.log(num);
+*/
+
+// Data Types
+
+// Number
+let accout_balance = 100;
+console.log(typeof accout_balance);
+
+// String
+let str = "Shubham";
+console.log(typeof str);
+
+console.log(`${str} has Rs.${accout_balance} balance in his accout.`);
+
+let comment = "Hello Coder Army";
+console.log(comment);
+
+// Boolean
+let statement = true;
+console.log(statement);
+console.log(typeof statement);
+let ofAge = false;
+console.log(ofAge);
+console.log(typeof ofAge);
+
+// Undefined
+let accout;
+console.log(accout);
+console.log(typeof accout);
+// null
+let balance = null;
+console.log(balance);
+console.log(typeof balance); // object
+
+// BigInt
+let a = 1546616446516748716954946594465196494619498149441949841484144498;
+console.log(a);
+a = 1546616446516748716954946594465196494619498149441949841484144498n;
+console.log(a);
+
+// Maximum Number in JavaScript
+console.log(Number.MAX_SAFE_INTEGER);
+// Minimum Number in JavaScript
+console.log(Number.MIN_SAFE_INTEGER);
