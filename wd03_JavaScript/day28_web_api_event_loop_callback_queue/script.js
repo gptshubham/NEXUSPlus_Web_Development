1e69bc167d85a273998e14ed03546fa106bf5ea9
gptshubham <shubhamca76@gmail.com>
2025-01-04 11:08:31 +0530

    explored web api, callback queue, and event loop

---
+++ wd03_JavaScript/day28_web_api_event_loop_callback_queue/script.js
@@ -0,0 +1,87 @@
+// Example 1
+
+/*
+
+console.log('Hello Coder Army');
+
+function meet() {
+  const arr = [2, 4, 6];
+  console.log(arr);
+}
+
+function greet() {
+  const a = 2 + 3;
+  console.log(a);
+  meet();
+  console.log(a + a);
+}
+
+greet();
+console.log('Program End');
+
+*/
+
+// Example 2
+
+/*
+console.log('Hello Coder Army');
+
+setTimeout(() => {
+  const a = 2 + 4;
+  console.log(a);
+}, 5000);
+
+let b = 20;
+let arr = [20, 30, 11];
+
+for (let i of arr) {
+  console.log(i * 20);
+}
+*/
+
+// Example 3
+
+/*
+console.log('Hello Coder Army');
+
+setTimeout(() => {
+  const a = 2 + 4;
+  console.log(a);
+}, 5000);
+
+setInterval(() => {
+  console.log('I am fast');
+}, 2000);
+
+let b = 20;
+let arr = [20, 30, 11];
+
+for (let i of arr) {
+  console.log(i * b);
+}
+  
+*/
+
+// Example 4
+
+/*
+let i = 1;
+const x = 10;
+const y = 20;
+j = 10000000000;
+while (i < j) {
+  i++;
+}
+
+setTimeout(() => {
+  console.log(x);
+}, 3000);
+
+setInterval(() => {
+  console.log(y);
+}, 1000);
+
+// console.log(count);
+*/
+
+// Example 5: fetch
