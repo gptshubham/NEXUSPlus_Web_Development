7474c623fd437bb14341e64a3ad939f94dfc5a09
gptshubham <shubhamca76@gmail.com>
2025-01-08 14:50:21 +0530

    revisited basics of git

---
+++ wd04_Git/script.js
@@ -0,0 +1,20 @@
+const username = 'Shubham';
+const age = 99;
+const userId = '1234';
+const balance = 10000;
+
+function updateBalance(value) {
+  return (balance += value);
+}
+
+console.log('Hello, world!');
+
+console.log('New Line');
+
+const c = 20 + 10;
+
+console.log(c);
+
+const h1 = document.querySelector('h1');
+
+h1.textContent = 'Captain Jack Sparrow';
