ed4d5dd218c46bf110f938e879b5d3c5da05b373
gptshubham <shubhamca76@gmail.com>
2025-01-12 13:59:16 +0530

    added function to show payment details on the screen

--- wd04_Git/day2/first.js
+++ wd04_Git/day2/first.js
@@ -1,11 +1,15 @@
 const username = 'Shubham';
 const age = 90;
-const balance = 300;
+let balance = 300;
+const creditors = {
+  creditor1: ['Rohan', 50],
+  creditor2: ['Mohan', 90],
+};
 
 console.log('Hello, world!');
 
-function updateBalance(balance) {
-  balance += 20;
+function updateBalance(amount) {
+  amount += 20;
 }
 
 updateBalance(balance);
@@ -21,6 +25,7 @@ function showUserData() {
   userAge.innerHTML = `Age: ${age}`;
 
   const balanceAmount = document.createElement('div');
+  balanceAmount.id = 'balance-container';
 
   balanceAmount.innerHTML = `Balance: ${balance}`;
 
@@ -29,3 +34,27 @@ function showUserData() {
 }
 
 showUserData();
+
+function showPaymentDetails() {
+  const paymentDetails = document.createElement('div');
+  paymentDetails.style.fontSize = '2rem';
+  paymentDetails.style.marginTop = '2rem';
+
+  const paymentTo = document.createElement('div');
+  const payment1 = `Amount Payable to Mr. ${creditors.creditor1[0]}: ${creditors.creditor1[1]}`;
+  balance -= creditors.creditor1[1];
+  const payment2 = `Amount Payable to Mr. ${creditors.creditor2[0]}: ${creditors.creditor2[1]}`;
+  balance -= creditors.creditor2[1];
+  paymentTo.innerHTML = `${payment1}<br>${payment2}`;
+  paymentDetails.append(paymentTo);
+  root.append(paymentDetails);
+}
+
+showPaymentDetails();
+
+function showUpdatedBalance(amount) {
+  const balanceElement = document.querySelector('#balance-container');
+  balanceElement.innerHTML = amount;
+}
+
+showUpdatedBalance(balance);
