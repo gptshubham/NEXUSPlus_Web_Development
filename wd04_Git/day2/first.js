3c8ef5a03bca54b8b7c7b59b6b3d933447ee1283
gptshubham <shubhamca76@gmail.com>
2025-01-12 13:24:04 +0530

    added function to show user data on screen

--- wd04_Git/day2/first.js
+++ wd04_Git/day2/first.js
@@ -19,3 +19,23 @@ console.log('Bug final commit');
 console.log('Captain America');
 
 console.log('Captain Cool MSD');
+
+const root = document.querySelector('.root');
+function showUserData() {
+  const userName = document.createElement('div');
+
+  userName.innerHTML = `Name: ${username}`;
+
+  const userAge = document.createElement('div');
+
+  userAge.innerHTML = `Age: ${age}`;
+
+  const balanceAmount = document.createElement('div');
+
+  balanceAmount.innerHTML = `Balance: ${balance}`;
+
+  root.append(userName, userAge, balanceAmount);
+  console.log(root);
+}
+
+showUserData();
