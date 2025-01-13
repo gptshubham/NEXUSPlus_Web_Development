66800f92464560a0447bb09661d6f2899ff3a44e
gptshubham <shubhamca76@gmail.com>
2025-01-13 12:19:04 +0530

    updated navigation buttons styles and functionality, additional message with GNU

--- wd04_Git/day3_git_fundamentals_part_3/script.js
+++ wd04_Git/day3_git_fundamentals_part_3/script.js
@@ -1,6 +1,4 @@
-const firstButton = document.querySelector('#nav-btn1');
-const secondButton = document.querySelector('#nav-btn2');
-const thirdButton = document.querySelector('#nav-btn3');
+const navigation = document.querySelector('#navigation');
 
 firstButton.addEventListener('click', () =>
   alert('You have clicked the first navigation button')
@@ -16,4 +14,10 @@ const geeksWorld = document.querySelector('.geeks-world');
 geeksWorld.addEventListener('click', () => {
   document.querySelector('header').classList.remove('hidden');
   geeksWorld.classList.add('hidden');
+navigation.addEventListener('click', (e) => {
+  if (e.target.id === 'log-in') {
+    alert('You have clicked log-in button');
+  } else if (e.target.id === 'sign-up') {
+    alert('You have clicked sign-up button');
+  }
 });
