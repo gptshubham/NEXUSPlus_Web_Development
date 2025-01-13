2b06d5e0964b6cc7738de8919da9f4ec3177548a
gptshubham <shubhamca76@gmail.com>
2025-01-13 13:52:32 +0530

    log-in branch final commit

--- wd04_Git/day3_git_fundamentals_part_3/script.js
+++ wd04_Git/day3_git_fundamentals_part_3/script.js
@@ -1,19 +1,11 @@
 const navigation = document.querySelector('#navigation');
 
-firstButton.addEventListener('click', () =>
-  alert('You have clicked the first navigation button')
-);
-secondButton.addEventListener('click', () =>
-  alert('You have clicked the second navigation button')
-);
-thirdButton.addEventListener('click', () =>
-  alert('You have clicked the third navigation button')
-);
-
 const geeksWorld = document.querySelector('.geeks-world');
 geeksWorld.addEventListener('click', () => {
   document.querySelector('header').classList.remove('hidden');
   geeksWorld.classList.add('hidden');
+});
+
 navigation.addEventListener('click', (e) => {
   if (e.target.id === 'log-in') {
     alert('You have clicked log-in button');
