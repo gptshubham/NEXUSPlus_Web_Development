a30bf8ca53ca68cd1513bcae36a691b6a57c2ad7
gptshubham <shubhamca76@gmail.com>
2025-01-13 12:04:53 +0530

    added js logic for navigation buttons

--- wd04_Git/day3_git_fundamentals_part_3/script.js
+++ wd04_Git/day3_git_fundamentals_part_3/script.js
@@ -1,1 +1,13 @@
+const firstButton = document.querySelector('#nav-btn1');
+const secondButton = document.querySelector('#nav-btn2');
+const thirdButton = document.querySelector('#nav-btn3');
 
+firstButton.addEventListener('click', () =>
+  alert('You have clicked the first navigation button')
+);
+secondButton.addEventListener('click', () =>
+  alert('You have clicked the second navigation button')
+);
+thirdButton.addEventListener('click', () =>
+  alert('You have clicked the third navigation button')
+);
